e9df9b1c00f89cfae3ff026474bfc29483585634
michimani <michimani210@gmail.com>
2022-03-13 20:33:33 +0900

    remove unsed function, add some tests #51

--- internal/params.go
+++ internal/params.go
@@ -4,7 +4,6 @@ import (
 	"io"
 	"net/url"
 	"strconv"
-	"strings"
 )
 
 type QueryParameter struct {
@@ -39,41 +38,6 @@ type EsaAPIParameter struct {
 	Query QueryParameterList
 	Path  PathParameterList
 	Body  io.Reader
-}
-
-func QueryValue(params []string) string {
-	if len(params) == 0 {
-		return ""
-	}
-
-	return strings.Join(params, ",")
-}
-
-func QueryString(paramsMap map[string]string, includes map[string]struct{}) string {
-	q := url.Values{}
-	for k, v := range paramsMap {
-		if _, ok := includes[k]; ok {
-			q.Add(k, v)
-		}
-	}
-
-	return q.Encode()
-}
-
-func GeneratePaginationParamsMap(p IPaginationParameters, paramsMap map[string]string) map[string]string {
-	if paramsMap == nil {
-		paramsMap = map[string]string{}
-	}
-
-	if page, ok := p.PageValue(); ok && page > 0 {
-		paramsMap["page"] = strconv.Itoa(page)
-	}
-
-	if perPage, ok := p.PerPageValue(); ok && perPage > 0 {
-		paramsMap["per_page"] = strconv.Itoa(perPage)
-	}
-
-	return paramsMap
 }
 
 func GeneratePaginationParameter(p IPaginationParameters) QueryParameterList {
