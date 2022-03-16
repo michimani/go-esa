c208d6c9db62b17945db3060fe79b9b128939eb0
michimani <michimani210@gmail.com>
2022-03-17 01:06:28 +0900

    add function's description #66

--- esaapi/oauth/api_token.go
+++ esaapi/oauth/api_token.go
@@ -11,6 +11,8 @@ const (
 	OAuthTokenInfoEndpoint = "https://api.esa.io/oauth/token/info"
 )
 
+// OAuthTokenInfoGet calls getting OAuth token information API.
+// GET /oauth/token/info
 func OAuthTokenInfoGet(ctx context.Context, c *gesa.GesaClient, p *types.OAuthTokenInfoGetParam) (*types.OAuthTokenInfoGetResponse, error) {
 	res := &types.OAuthTokenInfoGetResponse{}
 	if err := c.CallAPI(ctx, OAuthTokenInfoEndpoint, "GET", p, res); err != nil {
