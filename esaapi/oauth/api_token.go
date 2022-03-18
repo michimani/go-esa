35824133e0d34ff7795c3c9324c9e0c21c52c65c
michimani <michimani210@gmail.com>
2022-03-19 02:48:56 +0900

    change endpoint definition to private #76

--- esaapi/oauth/api_token.go
+++ esaapi/oauth/api_token.go
@@ -8,14 +8,14 @@ import (
 )
 
 const (
-	OAuthTokenInfoEndpoint = "https://api.esa.io/oauth/token/info"
+	oauthTokenInfoEndpoint = "https://api.esa.io/oauth/token/info"
 )
 
 // OAuthTokenInfoGet calls getting OAuth token information API.
 // GET /oauth/token/info
 func OAuthTokenInfoGet(ctx context.Context, c *gesa.GesaClient, p *types.OAuthTokenInfoGetParam) (*types.OAuthTokenInfoGetResponse, error) {
 	res := &types.OAuthTokenInfoGetResponse{}
-	if err := c.CallAPI(ctx, OAuthTokenInfoEndpoint, "GET", p, res); err != nil {
+	if err := c.CallAPI(ctx, oauthTokenInfoEndpoint, "GET", p, res); err != nil {
 		return nil, err
 	}
 
--- esaapi/teams/api_teams.go
+++ esaapi/teams/api_teams.go
@@ -8,15 +8,15 @@ import (
 )
 
 const (
-	TeamsGetEndpoint         = "https://api.esa.io/:esa_api_version/teams"
-	TeamsTeamNameGetEndpoint = "https://api.esa.io/:esa_api_version/teams/:team_name"
+	teamsGetEndpoint         = "https://api.esa.io/:esa_api_version/teams"
+	teamsTeamNameGetEndpoint = "https://api.esa.io/:esa_api_version/teams/:team_name"
 )
 
 // TeamsGet calls getting teams API.
 // GET /:esa_api_version/teams
 func TeamsGet(ctx context.Context, c *gesa.GesaClient, p *types.TeamsGetParam) (*types.TeamsGetResponse, error) {
 	res := &types.TeamsGetResponse{}
-	if err := c.CallAPI(ctx, TeamsGetEndpoint, "GET", p, res); err != nil {
+	if err := c.CallAPI(ctx, teamsGetEndpoint, "GET", p, res); err != nil {
 		return nil, err
 	}
 
@@ -27,7 +27,7 @@ func TeamsGet(ctx context.Context, c *gesa.GesaClient, p *types.TeamsGetParam) (
 // GET /:esa_api_version/teams/:team_name
 func TeamsTeamNameGet(ctx context.Context, c *gesa.GesaClient, p *types.TeamsTeamNameGetParam) (*types.TeamsTeamNameGetResponse, error) {
 	res := &types.TeamsTeamNameGetResponse{}
-	if err := c.CallAPI(ctx, TeamsTeamNameGetEndpoint, "GET", p, res); err != nil {
+	if err := c.CallAPI(ctx, teamsTeamNameGetEndpoint, "GET", p, res); err != nil {
 		return nil, err
 	}
 
