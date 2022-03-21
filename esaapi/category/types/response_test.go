81c6208c54fdf0743281fa668160fa4981648135
michimani <michimani210@gmail.com>
2022-03-21 11:21:39 +0900

    support POST v1/teams/:team_name/categories/batch_move #27

---
+++ esaapi/category/types/response_test.go
@@ -0,0 +1,124 @@
+package types_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/michimani/go-esa/esaapi/category/types"
+	"github.com/michimani/go-esa/gesa"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_BatchMoveOutput_SetRateLimitInfo(t *testing.T) {
+	resetTimestamp := gesa.Timestamp(100000000)
+
+	cases := []struct {
+		name string
+		h    http.Header
+		want *types.BatchMoveOutput
+	}{
+		{
+			name: "normal",
+			h: http.Header{
+				"X-RateLimit-Limit":     []string{"1"},
+				"X-RateLimit-Remaining": []string{"100"},
+				"X-RateLimit-Reset":     []string{"100000000"},
+			},
+			want: &types.BatchMoveOutput{
+				RateLimitInfo: &gesa.RateLimitInformation{
+					Limit:     1,
+					Remaining: 100,
+					Reset:     &resetTimestamp,
+				},
+			},
+		},
+		{
+			name: "normal: limit is empty",
+			h: http.Header{
+				"X-RateLimit-Limit":     []string{},
+				"X-RateLimit-Remaining": []string{"100"},
+				"X-RateLimit-Reset":     []string{"100000000"},
+			},
+			want: &types.BatchMoveOutput{
+				RateLimitInfo: &gesa.RateLimitInformation{
+					Limit:     0,
+					Remaining: 100,
+					Reset:     &resetTimestamp,
+				},
+			},
+		},
+		{
+			name: "normal: remaining is empty",
+			h: http.Header{
+				"X-RateLimit-Limit":     []string{"1"},
+				"X-RateLimit-Remaining": []string{},
+				"X-RateLimit-Reset":     []string{"100000000"},
+			},
+			want: &types.BatchMoveOutput{
+				RateLimitInfo: &gesa.RateLimitInformation{
+					Limit:     1,
+					Remaining: 0,
+					Reset:     &resetTimestamp,
+				},
+			},
+		},
+		{
+			name: "normal: reset is empty",
+			h: http.Header{
+				"X-RateLimit-Limit":     []string{"1"},
+				"X-RateLimit-Remaining": []string{"100"},
+				"X-RateLimit-Reset":     []string{},
+			},
+			want: &types.BatchMoveOutput{
+				RateLimitInfo: &gesa.RateLimitInformation{
+					Limit:     1,
+					Remaining: 100,
+					Reset:     nil,
+				},
+			},
+		},
+		{
+			name: "error: invalid rate limit limit value",
+			h: http.Header{
+				"X-RateLimit-Limit":     []string{"a"},
+				"X-RateLimit-Remaining": []string{"100"},
+				"X-RateLimit-Reset":     []string{"100000000"},
+			},
+			want: &types.BatchMoveOutput{
+				RateLimitInfo: nil,
+			},
+		},
+		{
+			name: "error: invalid rate limit remaining value",
+			h: http.Header{
+				"X-RateLimit-Limit":     []string{"1"},
+				"X-RateLimit-Remaining": []string{"a"},
+				"X-RateLimit-Reset":     []string{"100000000"},
+			},
+			want: &types.BatchMoveOutput{
+				RateLimitInfo: nil,
+			},
+		},
+		{
+			name: "error: invalid rate limit reset value",
+			h: http.Header{
+				"X-RateLimit-Limit":     []string{"1"},
+				"X-RateLimit-Remaining": []string{"100"},
+				"X-RateLimit-Reset":     []string{"a"},
+			},
+			want: &types.BatchMoveOutput{
+				RateLimitInfo: nil,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			asst := assert.New(tt)
+			res := &types.BatchMoveOutput{}
+			res.SetRateLimitInfo(c.h)
+
+			asst.Equal(c.want.RateLimitInfo, res.RateLimitInfo)
+		})
+	}
+}
