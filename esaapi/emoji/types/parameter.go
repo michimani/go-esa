73006dafa4a4c26791f89bf9ade30b076263367f
michimani <michimani210@gmail.com>
2022-03-21 12:25:36 +0900

    support POST /v1/teams/:team_name/emojis #35

--- esaapi/emoji/types/parameter.go
+++ esaapi/emoji/types/parameter.go
@@ -1,8 +1,10 @@
 package types
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/michimani/go-esa/internal"
 )
@@ -26,5 +28,54 @@ func (p *ListEmojisInput) EsaAPIParameter() (*internal.EsaAPIParameter, error) {
 		Path:  pp,
 		Query: internal.QueryParameterList{},
 		Body:  nil,
+	}, nil
+}
+
+type CreateEmojiInput struct {
+	TeamName string // required
+
+	Code       string  // required
+	OriginCode *string // required if you add the other alias to already exists emoji
+	Image      *string // required if you create new emoji, BASE64 encoded string
+}
+
+type createEmojiPayload struct {
+	Emoji createEmojiPayloadEmoji `json:"emoji"`
+}
+
+type createEmojiPayloadEmoji struct {
+	Code       string  `json:"code"`
+	OriginCode *string `json:"origin_code,omitempty"`
+	Image      *string `json:"image,omitempty"`
+}
+
+func (p *CreateEmojiInput) EsaAPIParameter() (*internal.EsaAPIParameter, error) {
+	if p == nil {
+		return nil, errors.New(internal.ErrorParameterIsNil)
+	}
+
+	pp := internal.PathParameterList{}
+	if p.TeamName == "" || p.Code == "" {
+		return nil, fmt.Errorf(internal.ErrorRequiredParameterEmpty, "CreateEmojiInput.TeamName, CreateEmojiInput.Code")
+	}
+	pp = append(pp, internal.PathParameter{Key: ":team_name", Value: p.TeamName})
+
+	payload := &createEmojiPayload{
+		Emoji: createEmojiPayloadEmoji{
+			Code:       p.Code,
+			OriginCode: p.OriginCode,
+			Image:      p.Image,
+		},
+	}
+
+	json, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return &internal.EsaAPIParameter{
+		Path:  pp,
+		Query: internal.QueryParameterList{},
+		Body:  strings.NewReader(string(json)),
 	}, nil
 }
--- esaapi/emoji/types/parameter_test.go
+++ esaapi/emoji/types/parameter_test.go
@@ -1,9 +1,11 @@
 package types_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/michimani/go-esa/esaapi/emoji/types"
+	"github.com/michimani/go-esa/gesa"
 	"github.com/michimani/go-esa/internal"
 	"github.com/stretchr/testify/assert"
 )
@@ -30,6 +32,117 @@ func Test_ListEmojisInput_EsaAPIParameter(t *testing.T) {
 		{
 			name:    "ng: has no required parameter",
 			p:       &types.ListEmojisInput{},
+			expect:  nil,
+			wantErr: true,
+		},
+		{
+			name:    "ng: nil",
+			p:       nil,
+			expect:  nil,
+			wantErr: true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			asst := assert.New(tt)
+			ep, err := c.p.EsaAPIParameter()
+			if c.wantErr {
+				asst.Error(err)
+				asst.Nil(ep)
+				return
+			}
+			assert.Equal(tt, c.expect, ep)
+		})
+	}
+}
+
+func Test_CreateEmojiInput_EsaAPIParameter(t *testing.T) {
+	cases := []struct {
+		name    string
+		p       *types.CreateEmojiInput
+		expect  *internal.EsaAPIParameter
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			p: &types.CreateEmojiInput{
+				TeamName: "test-team",
+				Code:     "test-code",
+			},
+			expect: &internal.EsaAPIParameter{
+				Path: internal.PathParameterList{
+					{Key: ":team_name", Value: "test-team"},
+				},
+				Query: internal.QueryParameterList{},
+				Body:  strings.NewReader(`{"emoji":{"code":"test-code"}}`),
+			},
+		},
+		{
+			name: "ok: with origin_code",
+			p: &types.CreateEmojiInput{
+				TeamName:   "test-team",
+				Code:       "test-code",
+				OriginCode: gesa.String("test-origin-code"),
+			},
+			expect: &internal.EsaAPIParameter{
+				Path: internal.PathParameterList{
+					{Key: ":team_name", Value: "test-team"},
+				},
+				Query: internal.QueryParameterList{},
+				Body:  strings.NewReader(`{"emoji":{"code":"test-code","origin_code":"test-origin-code"}}`),
+			},
+		},
+		{
+			name: "ok: with image",
+			p: &types.CreateEmojiInput{
+				TeamName: "test-team",
+				Code:     "test-code",
+				Image:    gesa.String("test-image"),
+			},
+			expect: &internal.EsaAPIParameter{
+				Path: internal.PathParameterList{
+					{Key: ":team_name", Value: "test-team"},
+				},
+				Query: internal.QueryParameterList{},
+				Body:  strings.NewReader(`{"emoji":{"code":"test-code","image":"test-image"}}`),
+			},
+		},
+		{
+			name: "ok: with all",
+			p: &types.CreateEmojiInput{
+				TeamName:   "test-team",
+				Code:       "test-code",
+				OriginCode: gesa.String("test-origin-code"),
+				Image:      gesa.String("test-image"),
+			},
+			expect: &internal.EsaAPIParameter{
+				Path: internal.PathParameterList{
+					{Key: ":team_name", Value: "test-team"},
+				},
+				Query: internal.QueryParameterList{},
+				Body:  strings.NewReader(`{"emoji":{"code":"test-code","origin_code":"test-origin-code","image":"test-image"}}`),
+			},
+		},
+		{
+			name: "ng: not has required parameter: has only TeamName",
+			p: &types.CreateEmojiInput{
+				TeamName: "test-team",
+			},
+			expect:  nil,
+			wantErr: true,
+		},
+		{
+			name: "ng: not has required parameter: has only Code",
+			p: &types.CreateEmojiInput{
+				Code: "test-code",
+			},
+			expect:  nil,
+			wantErr: true,
+		},
+		{
+			name:    "ng: not has required parameter",
+			p:       &types.CreateEmojiInput{},
 			expect:  nil,
 			wantErr: true,
 		},
